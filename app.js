d936da7592bed2145e2778a69b5c761e8779f9e0
Dania <27072003danya@gmail.com>
2023-02-09 01:44:43 +0200

    added logic for the button like

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 let productsCountEl = document.getElementById("product-count");
 let addToCartBtns = document.querySelectorAll(".btn-add-to-cart");
 
+let likeEl = document.querySelectorAll(".like");
+
+
 addToCartBtns.forEach((elem) => {
     elem.addEventListener("click", function(){
         // let prevProductsCount = Number(productsCountEl.textContent);
@@ -21,3 +24,16 @@ addToCartBtns.forEach((elem) => {
 //         console.log("clicked");
 //     })
 // }
+
+likeEl.forEach((item) => {
+    item.addEventListener("click", function(){
+        if(item.style.backgroundColor === "" || item.style.backgroundColor === "transparent"){
+            item.style.backgroundColor = "#2c71b8";
+            item.style.backgroundImage = "url(images/white-like.png)"
+        } else {
+            item.style.backgroundColor = "transparent";
+            item.style.backgroundImage = "url(images/blue-like.png)"
+        }
+    })
+})
+
