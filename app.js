18478c65af159f4eb3d0dbf59747d7514e3c5edb
Dania <27072003danya@gmail.com>
2023-02-13 19:50:07 +0200

    added stylization slider

--- app.js
+++ app.js
@@ -2,7 +2,41 @@ let productsCountEl = document.getElementById("product-count");
 let addToCartBtns = document.querySelectorAll(".btn-add-to-cart");
 
 let likeBtns = document.querySelectorAll(".like-btn");
-console.log(likeBtns);
+
+let moreDetailsBtns = document.querySelectorAll(".btn-more-details");
+
+let modal = document.querySelector(".modal");
+
+let closeBtn = document.querySelector(".btn-close");
+
+
+modal.addEventListener("click", function(e){
+    // console.log(e);
+    // console.log(e.target);
+    if(e.target === modal){
+        modal.classList.remove("show");
+    }
+
+})
+
+function showModalByScroll(){
+    if(window.scrollY >= document.body.scrollHeight / 2){
+        modal.classList.add("show");
+        window.removeEventListener("scroll", showModalByScroll)
+    }
+}
+
+window.addEventListener("scroll", showModalByScroll);
+
+closeBtn.addEventListener("click", function(){
+    modal.classList.remove("show");
+})
+
+moreDetailsBtns.forEach((item) => {
+    item.addEventListener("click", function(){
+        modal.classList.add("show");
+    })
+})
 
 addToCartBtns.forEach((elem) => {
     elem.addEventListener("click", function(){
@@ -38,5 +72,12 @@ likeBtns.forEach((item) => {
 })
 
 
+// slick
 
-$(".slider-block").slick();
+$(".slider-block").slick({
+    dots: true,
+    prevArrow: "<img src='images/arrow_left.png' class='prev' alt='1'>",
+    nextArrow: "<img src='images/arrow_right.png' class='next' alt='2'>",
+});
+
+AOS.init();
